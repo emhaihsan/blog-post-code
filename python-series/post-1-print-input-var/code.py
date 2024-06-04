1018e5b83b198503366478fa24afa6b861457aa0
Muhammad Ihsan <emhihsan@gmail.com>
2024-06-04 21:47:20 +0700

    post 1 basic print and input

---
+++ python-series/post-1-print-input-var/code.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+
+print("Hello", "World", "!")
+
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+age = int(input("How old are you? "))
+print("You are " + str(age) + " years old.")
+
+name = "Alice"
+age = 25
+
+print("Name:", name)
+print("Age:", age)
+
+age = age + 1
+print("Next year, you will be", age, "years old.")
